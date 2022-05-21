b560aa55595afbf7928107dc1526e29ce3cb84c2
Piao <pakushiken@gmail.com>
2022-05-21 21:22:35 +0900

    update

--- main.rb
+++ main.rb
@@ -4,107 +4,96 @@ require 'slack/incoming/webhooks'
 
 Dotenv.load
 
-INTERVAL_TIME_CALC_AVG = 5
-INTERVAL_TIME_DO = 60 * 10
-
+SYMBOL = 'BNBBUSD'
 QUANTITY_BNB = 0.025
 QUANTITY_BUSD = 12
-SYMBOL = 'BNBBUSD'
-SELL_ORDER_MAX = 50
-BUY_ORDER_MAX = 50
+SHORT_CANDLE_NUM = 7
+LONG_CANDLE_NUM = 30
+KLINE_INTERVAL = '5m'
 
 client = Binance::Spot.new(key: ENV['KEY'], secret: ENV['SECRET'])
 slack = Slack::Incoming::Webhooks.new ENV['WEBHOOK_URL']
 
-def can_sell?(free_balance_bnb, sell_orders)
-  return false if sell_orders.count >= SELL_ORDER_MAX
+# 最新の価額でMAを計算
+def calc_ma(candles, candle_num)
+  closes = []
+  last_candles = candles.last(candle_num)
+  last_candles.each do |candle|
+    closes.push(candle[4].to_f)
+  end
+  closes.sum / closes.size
+end
 
+# 一個前のcandleまでの価額でMAを計算
+def calc_ma_before(candles, candle_num)
+  closes = []
+  last_candles = candles.last(candle_num + 1)
+
+  last_candles.pop
+  last_candles.each do |candle|
+    closes.push(candle[4].to_f)
+  end
+  closes.sum / closes.size
+end
+
+def check_cross(ma5, ma15, ma5_before, ma15_before)
+  if ma5_before < ma15_before && ma5 > ma15
+    'golden'
+  elsif ma5_before > ma15_before && ma5 < ma15
+    'dead'
+  end
+end
+
+def can_sell?(free_balance_bnb)
   free_balance_bnb > QUANTITY_BNB
 end
 
-def can_buy?(free_balance_busd, buy_orders)
-  return false if buy_orders.count >= BUY_ORDER_MAX
-
+def can_buy?(free_balance_busd)
   free_balance_busd > QUANTITY_BUSD
 end
 
-# sell注文が多い時sell_priceとbuy_priceを上げる
-# sell注文が少ない時sell_priceとbuy_priceを上げる
-def calc_price(avg_price, buy_orders, sell_orders)
-  sell_rate = 0.5
-  if (buy_orders.count + sell_orders.count) > 3
-    sell_rate = sell_orders.count.to_f / (buy_orders.count + sell_orders.count)
-  end
-  p "Sell rate: #{sell_rate}"
-  sell_price = nil
-  buy_price = nil
-
-  if sell_rate >= 0.7
-    sell_price = avg_price * 1.001
-    buy_price = avg_price * 0.997
-  elsif sell_rate <= 0.3
-    sell_price = avg_price * 1.0012
-    buy_price = avg_price * 0.9999
-  else
-    sell_price = avg_price * 1.001
-    buy_price = avg_price * 0.999
-  end
-  { sell_price: sell_price, buy_price: buy_price }
-end
-
-while true
+loop do
+  sleep 5
   begin
-    p '===== START ====='
-    p Time.now
-    prices = []
+    candles = client.klines(symbol: SYMBOL, interval: KLINE_INTERVAL, startTime: (Time.now.to_i - 3600 * 24) * 1000)
+    ma5 = calc_ma(candles, SHORT_CANDLE_NUM)
+    ma15 = calc_ma(candles, LONG_CANDLE_NUM)
+    ma5_before = calc_ma_before(candles, SHORT_CANDLE_NUM)
+    ma15_before = calc_ma_before(candles, LONG_CANDLE_NUM)
 
-    balance_bnb = client.account[:balances].select { |bal| bal[:asset] == 'BNB' }.first
-    free_balance_bnb = balance_bnb[:free].to_f
-    locked_balance_bnb = balance_bnb[:locked].to_f
-    balance_busd = client.account[:balances].select { |bal| bal[:asset] == 'BUSD' }.first
-    free_balance_busd = balance_busd[:free].to_f
-    locked_balance_busd = balance_busd[:locked].to_f
+    p "MA5:#{ma5.round(3)}, MA15:#{ma15.round(3)}"
+    cross_flag = check_cross(ma5, ma15, ma5_before, ma15_before)
 
-    p "Free BNB: #{free_balance_bnb}"
-    p "Free BUSD: #{free_balance_busd}"
-    10.times do
-      sleep INTERVAL_TIME_CALC_AVG
-      last_price = client.ticker_24hr(symbol: SYMBOL)[:lastPrice].to_f
-      prices.push last_price
+    if cross_flag == 'golden'
+      p 'golden'
+      balance_busd = client.account[:balances].select { |bal| bal[:asset] == 'BUSD' }.first
+      free_balance_busd = balance_busd[:free].to_f
+      if free_balance_busd > 12
+        last_price = client.ticker_24hr(symbol: SYMBOL)[:lastPrice].to_f
+        p "last_price:#{last_price},free_balance_busd:#{free_balance_busd} "
+        quantity = (free_balance_busd / last_price).to_f.floor(3)
+        price = last_price.to_f.round(1)
+        response = client.new_order(symbol: SYMBOL, side: 'BUY', price: price, quantity: quantity,
+                                    type: 'LIMIT', timeInForce: 'GTC')
+        p response
+        slack.post "[Buy] price: #{price}, quantity: #{quantity}"
+      end
+    elsif cross_flag == 'dead'
+      p 'dead'
+
+      balance_bnb = client.account[:balances].select { |bal| bal[:asset] == 'BNB' }.first
+      free_balance_bnb = balance_bnb[:free].to_f.floor(3)
+      if free_balance_bnb > 0.025
+        last_price = client.ticker_24hr(symbol: SYMBOL)[:lastPrice].to_f
+        price = last_price.to_f.floor(1)
+        response = client.new_order(symbol: SYMBOL, side: 'SELL', price: price, quantity: free_balance_bnb,
+                                    type: 'LIMIT', timeInForce: 'GTC')
+
+        p response
+        slack.post "[Sell] price: #{price}, quantity: #{free_balance_bnb}"
+      end
     end
-
-    orders = client.open_orders(symbol: SYMBOL)
-    buy_orders = orders.select { |o| o[:side] == 'BUY' }
-    sell_orders = orders.select { |o| o[:side] == 'SELL' }
-
-    avg_price = prices.sum / prices.size
-    price = calc_price(avg_price, buy_orders, sell_orders)
-    buy_price = price[:buy_price] > prices.last ? prices.last : price[:buy_price]
-    buy_price = buy_price.round(1)
-    sell_price = price[:sell_price] < prices.last ? prices.last : price[:sell_price]
-    sell_price = sell_price.round(1)
-
-    p "avg_price:#{avg_price}"
-    p "buy_price:#{buy_price}"
-    p "sell_price:#{sell_price}"
-
-    if can_sell?(free_balance_bnb, sell_orders) && can_buy?(free_balance_busd, buy_orders)
-      response_sell = client.new_order(symbol: SYMBOL, side: 'SELL', price: sell_price, quantity: QUANTITY_BNB,
-                                       type: 'LIMIT', timeInForce: 'GTC')
-      p response_sell
-      response_buy = client.new_order(symbol: SYMBOL, side: 'BUY', price: buy_price, quantity: QUANTITY_BNB,
-                                      type: 'LIMIT', timeInForce: 'GTC')
-      p response_buy
-      slack.post "BuyPrice: #{response_buy[:price].to_f.round(1)}, SellPrice:#{response_sell[:price].to_f.round(1)} Avg:#{avg_price.round(1)}"
-      slack.post "Balance: BNB=>#{
-        free_balance_bnb.to_f.round(2) + locked_balance_bnb.to_f
-      } + BUSD=>#{free_balance_busd.to_f.round(2) + locked_balance_busd.to_f.round(2)} = #{((free_balance_bnb.to_f.round(2) + locked_balance_bnb.to_f.round(2)) * avg_price + free_balance_busd.to_f + locked_balance_busd.to_f).round(2)}USD"
-
-    end
-    p '===== END ====='
   rescue StandardError => e
-    p e
-    slack.post "<!channel>#{e}"
+    slack.post e.to_s
   end
-  sleep INTERVAL_TIME_DO
 end
